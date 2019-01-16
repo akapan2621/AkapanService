00f79c6ab64fa46d4386f7ac2b52072be7dc7037
akapan <akapan.r@hitachi-tstv.com>
2019-01-16 09:47:46 +0700

    Check space success

--- AkapanService/RegisterViewController.swift
+++ AkapanService/RegisterViewController.swift
@@ -9,22 +9,55 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
+    
+//    Explicit
+    var name: String?
+    var user: String?
+    var password: String?
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }   // Main Function
+
+    @IBAction func cloudUploadButton(_ sender: Any) {
+        
+//        Get value from TextFields
+        name = nameTextField.text
+        user = userTextField.text
+        password = passwordTextField.text
+        
+//        Call show log
+        showLog(title: "Name", message: name!)
+        showLog(title: "User", message: user!)
+        showLog(title: "Password", message: password!)
+        
+//        Call check space
+        checkSpace(name: name!, user: user!, password: password!)
+        
+    }   // CloudUpload
+    
+    func checkSpace(name: String,user: String, password: String) -> Void {
+        
+        if (name.count == 0) || (user.count == 0) || (password.count == 0) {
+//            Have space
+            showLog(title: "Have space", message: "")
+        } else {
+//            No space
+            showLog(title: "No space", message: "")
+        }
+        
+    }
+    func showLog(title: String, message: String) -> Void {
+        print("\(title) = \(message)")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+}   // Main Class
